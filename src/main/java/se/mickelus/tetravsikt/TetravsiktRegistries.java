fae83babc462848204eea3d40a888bec3dc64e11
Mikael E. Wikner <wikner.mikael@gmail.com>
2025-03-19 16:54:01 +0100

    added texture for coal mix

--- src/main/java/se/mickelus/tetravsikt/TetravsiktRegistries.java
+++ src/main/java/se/mickelus/tetravsikt/TetravsiktRegistries.java
@@ -1,17 +1,47 @@
 package se.mickelus.tetravsikt;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class TetravsiktRegistries {
     public static final DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, TetravsiktMod.MOD_ID);
+    public static final DeferredRegister<CreativeModeTab> creativeTabs = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TetravsiktMod.MOD_ID);
+    private static RegistryObject<CreativeModeTab> defaultCreativeTabs;
+
+    static RegistryObject<Item> ironCoalMix;
 
     public static void init(IEventBus bus) {
         bus.register(TetravsiktRegistries.class);
         items.register(bus);
+        creativeTabs.register(bus);
 
-//        items.register(DummyKnifeBladeItem.identifier, DummyKnifeBladeItem::new);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // CREATIVE TABS
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        defaultCreativeTabs = creativeTabs.register("default", () -> CreativeModeTab.builder()
+                .icon(() -> new ItemStack(ironCoalMix.get()))
+                .title(Component.translatable("itemGroup.tetravsikt"))
+                .build());
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // ITEMS
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ironCoalMix = items.register("iron_coal_mix", () -> new Item(new Item.Properties()));
+    }
+
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == defaultCreativeTabs.getKey()) {
+            event.accept(ironCoalMix);
+        }
     }
 }
